dd35704b242b9546ffc2507812d175a8538d155c
Shiva Kumar G <shivakumarg06@gmail.com>
2019-03-05 13:08:14 +0530

    Update automate-iis.ps1

--- automate-iis.ps1
+++ automate-iis.ps1
@@ -22,21 +22,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Install IIS
 Add-WindowsFeature Web-Server
-
-# Create directory and default page for Contoso
-New-Item -Path "C:\inetpub\wwwroot\contoso" -Type "Directory"
-Set-Content -Path "C:\inetpub\wwwroot\contoso\Default.htm" -Value "Contoso website on host $($env:computername) !"
-
-# Create IIS app pool and web site for Contoso
-New-WebAppPool -Name "ContosoAppPool"
-New-WebSite -Name "Contoso Web Site" -IPAddress "*" -Port 80 -HostHeader "contoso" -PhysicalPath "C:\inetpub\contoso" -ApplicationPool "ContosoAppPool"
-
-# Create directory and default page for Fabrikam
-New-Item -Path "C:\inetpub\wwwroot\fabrikam" -Type "Directory"
-Set-Content -Path "C:\inetpub\wwwroot\fabrikam\Default.htm" -Value "Fabrikam website on host $($env:computername) !"
-
-# Create IIS app pool and web site for Contoso
-New-WebAppPool -Name "FabrikamAppPool"
-New-WebSite -Name "Fabrikam Web Site" -IPAddress "*" -Port 80 -HostHeader "fabrikam" -PhysicalPath "C:\inetpub\fabrikam" -ApplicationPool "FabrikamAppPool"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
