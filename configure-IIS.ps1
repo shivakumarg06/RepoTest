aaa0d4fd40069be8f88725f745e8605c60fc1e29
Shiva Kumar G <shivakumarg06@gmail.com>
2019-03-05 12:25:43 +0530

    Update configure-IIS.ps1

--- configure-IIS.ps1
+++ configure-IIS.ps1
@@ -1,18 +1,3 @@
-# SetupWeb.ps1
-# Azure DevOps Pipelines Variables 
-$sourceDir = "$(System.DefaultWorkingDirectory)\$(Build.DefinitionName)\"
-
-$ArchivedFolders = "$sourceDir\drop"
-$workDir = "C:\verimoto"
-
-New-item -Path C:\verimoto -ItemType Directory -Force
-#Expand-Archive -Path $ArchivedFolders\Verimoto.Admin -DestinationPath $ArchivedFolders\Verimoto.Admin
-#Expand-Archive -Path $ArchivedFolders\
-Get-ChildItem $ArchivedFolders -Filter *.zip | Expand-Archive -DestinationPath  "$workDir\UnZipped" -Force
-#Copy-Item $workDir C:\verimoto\ -recurse -Force
-Copy-Item "$workDir\UnZipped\Content\C_C\VSTSAgent\vsts-agent-win-x64-2.147.1\_work\1\s\Verimoto\*" -Destination "$workDir" -Recurse -Force
-
-
 Set-ExecutionPolicy Unrestricted
 
 if(Get-Website -Name "Default Web Site")
